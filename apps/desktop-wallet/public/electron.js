c9528c1bab8b548e5f3a1f72f20aeeb274b13a4b
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-10-30 17:18:54 +0100

    Use app.whenReady instead of app.on("ready")

--- apps/desktop-wallet/public/electron.js
+++ apps/desktop-wallet/public/electron.js
@@ -245,10 +245,8 @@ app.on('second-instance', (_event, args) => {
   }
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', async function () {
+// Prefer app.whenReady() over app.on('ready'): https://github.com/electron/electron/pull/21972
+app.whenReady().then(async () => {
   if (isDev) {
     const {
       default: { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS }
@@ -332,19 +330,17 @@ app.on('ready', async function () {
   })
 
   createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow()
+  })
 })
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+  // On macOS it is common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q
   if (!isMac) app.quit()
-})
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
 })
 
 app.on('open-url', (_, url) => {
